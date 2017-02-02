0a1ef6a5a18e0ed33c99c9b8b3c25f90e98754fc
TonyForest <foretony@gmail.com>
2017-02-01 20:15:50 -0700

    First commit

---
+++ src/pi_mc/PI_MC.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pi_mc;
+
+/**
+ *
+ * @author systouf
+ */
+public class PI_MC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println(Math.random()); // Display the string.
+       
+    }
+    
+}
